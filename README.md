72403836952e668465b68423b7c22ea764fe7e4a
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-04-25 16:41:06 +0200

    updating v0 from master (#1)
    * First version of README
    
    * first version
    
    * first version
    
    * first install.sh
    
    We use the v0 branch unless user decides otherwise
    
    * first stop script
    
    * first uninstall script
    
    * first version of update script
    
    * new daemon 21

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # domod
-Domotica 
+Platform independent domotica data gatherer
+
+# Installing
+
+```
+sudo su -
+cd /path/to/where/you/want/store/domod
+git clone https://github.com/Mausy5043/domod.git
+cd domod
+./install.sh
+./update.sh
+```
+
+# Attribution
+The python code for the daemons is based on previous work by
+- [Charles Menguy](http://stackoverflow.com/questions/10217067/implementing-a-full-python-unix-style-daemon-process)
+- [Sander Marechal](http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/)
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# this repo gets installed either manually by the user or automatically by
+# a `*boot` repo.
+
+ME=$(whoami)
+
+echo -n "Started installing DOMOD on "; date
+minit=$(echo $RANDOM/555 |bc)
+echo "MINIT = "$minit
+pushd "$HOME/domod"
+  # To suppress git detecting changes by chmod:
+  git config core.fileMode false
+  # set the branch
+  if [ ! -e "$HOME/.domod.branch" ]; then
+    echo "v0" > "$HOME/.domod.branch"
+  fi
+
+  # Create the /etc/cron.d directory if it doesn't exist
+  sudo mkdir -p /etc/cron.d
+  # Set up some cronjobs
+  echo "# m h dom mon dow user  command" | sudo tee /etc/cron.d/domod
+  echo "$MINIT  * *   *   *   $ME    $HOME/domod/update.sh 2>&1 | logger -p info -t domod" | sudo tee --append /etc/cron.d/domod
+  # @reboot we allow for 120s for the WiFi to come up:
+  echo "@reboot               $ME    sleep 120; $HOME/domod/update.sh 2>&1 | logger -p info -t domod" | sudo tee --append /etc/cron.d/domod
+
+popd
+
+echo -n "Finished installation of DOMOD on "; date
