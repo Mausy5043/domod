291746972d3a1332f14de22a1dee752485769fb7
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-25 17:41:21 +0200

    graphing 21

--- again98d.py
+++ again98d.py
@@ -109,9 +109,9 @@ def write_lftp(script):
     f.write('# This file is created automatically by ' + MYAPP + '\n\n')
     f.write('# lftp script\n\n')
     f.write('open hendrixnet.nl;\n')
-    f.write('cd /public_html/grav/user/pages/04.status/;\n')
-    f.write('mkdir -p -f _' + NODE + ' ;\n')
-    f.write('cd _' + NODE + ' ;\n')
+    f.write('cd /public_html/grav/user/pages/03.again/;\n')
+    # f.write('mkdir -p -f _' + NODE + ' ;\n')
+    # f.write('cd _' + NODE + ' ;\n')
     f.write('mirror --reverse --delete --verbose=3 -c /tmp/' + MYAPP + '/site/ . ;\n')
     f.write('\n')
 
