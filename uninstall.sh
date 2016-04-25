8b3c6e5591ee42d886b5d9784a3e1d981765758b
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-25 15:32:53 +0200

    first uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this repo gets installed either manually by the user or automatically by
+# a `*boot` repo.
+# The hostname is in /etc/hostname prior to running `install.sh` here!
+
+ME=$(whoami)
+HOSTNAME=$(cat /etc/hostname)
+
+echo -n "Started UNinstalling DOMOD on "; date
+
+pushd "$HOME/domod"
+ source ./includes
+
+
+  sudo rm /etc/cron.d/domod
+
+  echo "  Stopping all diagnostic daemons"
+  for daemon in $againlist; do
+    echo "Stopping "$daemon
+    eval "./again"$daemon"d.py stop"
+  done
+  echo "  Stopping all service daemons"
+  for daemon in $srvclist; do
+    echo "Stopping "$daemon
+    eval "./again"$daemon"d.py stop"
+  done
+popd
+
+echo -n "Finished UNinstallation of DOMOD on "; date
