c02f4e76eca140a3f726bba9aad2ea550e353299
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-08 10:34:12 +0200

    separate graphing script

---
+++ graphs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pull data from MySQL server and graph them.
+
+LOCAL=$(date)
+LOCALSECONDS=$(date -d "$LOCAL" +%s)
+UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
+UTCSECONDS=$(date -d "$UTC" +%s)
+UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
+
+pushd $HOME/lnxdiagd >/dev/null
+  if [ $(cat /tmp/sql21d.csv | wc -l) -gt 30 ]; then
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
+  fi
+  if [ $(cat /tmp/sql22d.csv | wc -l) -gt 30 ]; then
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
+  fi
+popd >/dev/null
