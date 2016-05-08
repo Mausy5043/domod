35978987498dd188ed107df074e1e30d06290650
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-08 10:40:40 +0200

    pushd to correct dir

--- graphs.sh
+++ graphs.sh
@@ -8,7 +8,7 @@ UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
 UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
 
-pushd $HOME/lnxdiagd >/dev/null
+pushd $HOME/domod >/dev/null
   if [ $(cat /tmp/domod/mysql/sql21d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
   fi
