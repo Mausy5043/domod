d0b2db3f716abfc1a66693e984624e236e16e227
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-08 10:33:38 +0200

    get mysql data for last hour, day and week

---
+++ getsqlweek.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Pull data from MySQL server and graph them.
+
+LOCAL=$(date)
+LOCALSECONDS=$(date -d "$LOCAL" +%s)
+UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
+UTCSECONDS=$(date -d "$UTC" +%s)
+UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
+datastore="/tmp/lnxdiagd/mysql"
+
+if [ ! -d "$datastore" ]; then
+  mkdir -p "$datastore"
+fi
+
+interval="INTERVAL 7 DAY "
+host=$(hostname)
+
+pushd $HOME/lnxdiagd >/dev/null
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM ds18 where (sample_time >=NOW() - $interval);" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql21w.csv"
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM dht22 where (sample_time >=NOW() - $interval);" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql22w.csv"
+  #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
+  #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > $datastore/sql2c.csv
+
+popd >/dev/null
