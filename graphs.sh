35f73c388577b60650126684ca08a9b57d755668
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-15 21:28:04 +0200

    no threading because of CPU saturation

--- graphs.sh
+++ graphs.sh
@@ -10,15 +10,12 @@ UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
 
 pushd "$HOME/domod" >/dev/null
   if [ $(wc -l < /tmp/domod/mysql/sql21d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp &
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
   fi
   if [ $(wc -l < /tmp/domod/mysql/sql22d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp &
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
   fi
   if [ $(wc -l < /tmp/domod/mysql/sql23d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp &
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp
   fi
-
-  wait
-  
 popd >/dev/null
