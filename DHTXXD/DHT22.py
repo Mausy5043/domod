ba9ab45cb300c3ce5929e6227417d1d23fcd3b41
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-08-10 18:51:23 +0200

    Kamstrup (#3)
    * python3 `print`ing

--- DHTXXD/DHT22.py
+++ DHTXXD/DHT22.py
@@ -2,10 +2,7 @@
 
 # 2014-07-11 DHT22.py
 
-import time
 import atexit
-
-import pigpio
 
 class sensor:
    """
@@ -243,40 +240,30 @@ class sensor:
 
 if __name__ == "__main__":
 
-   import time
+  import time
+  import pigpio
+  import DHT22
 
-   import pigpio
+  # Intervals of about 2 seconds or less will eventually hang the DHT22.
+  INTERVAL = 3
 
-   import DHT22
+  pi = pigpio.pi()
+  s  = DHT22.sensor(pi, 22, LED=16, power=8)
+  r  = 0
+  next_reading = time.time()
 
-   # Intervals of about 2 seconds or less will eventually hang the DHT22.
-   INTERVAL = 3
+  while True:
+    r += 1
+    s.trigger()
+    time.sleep(0.2)
+    print("{} {} {} {:3.2f} {} {} {} {}".format(
+      r, s.humidity(), s.temperature(), s.staleness(),
+      s.bad_checksum(), s.short_message(), s.missing_message(),
+      s.sensor_resets()))
 
-   pi = pigpio.pi()
+    next_reading += INTERVAL
+    time.sleep(next_reading-time.time())  # Overall INTERVAL second polling.
 
-   s = DHT22.sensor(pi, 22, LED=16, power=8)
+  s.cancel()
 
-   r = 0
-
-   next_reading = time.time()
-
-   while True:
-
-      r += 1
-
-      s.trigger()
-
-      time.sleep(0.2)
-
-      print("{} {} {} {:3.2f} {} {} {} {}".format(
-         r, s.humidity(), s.temperature(), s.staleness(),
-         s.bad_checksum(), s.short_message(), s.missing_message(),
-         s.sensor_resets()))
-
-      next_reading += INTERVAL
-
-      time.sleep(next_reading-time.time())  # Overall INTERVAL second polling.
-
-   s.cancel()
-
-   pi.stop()
+  pi.stop()
